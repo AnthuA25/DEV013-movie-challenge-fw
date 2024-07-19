40b772e05576fc41f5a37b6e3d6ea23ec7c08cd8
Aylin <aylin_santacruz@hotmail.com>
2024-07-19 17:45:17 -0500

    feat:Add project structure

---
+++ dev013-movie-challenge/src/Home.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+// import {CarruselMovie} from './components/CarruselMovie'
+
+function Home() {
+
+
+  return (
+    <>
+      <div className='NavBar'>
+        <h4>WatchMovie</h4>
+      </div>
+    </>
+  )
+}
+
+export default Home
