1665b4be1d1cb6d80393c8a581ed7ff538d91e50
Aylin <aylin_santacruz@hotmail.com>
2024-08-21 00:19:38 -0500

    feat:Create MovieCard component with default poster handling and movie details

--- dev013-movie-challenge/src/components/MovieCard.tsx
+++ dev013-movie-challenge/src/components/MovieCard.tsx
@@ -9,6 +9,12 @@ interface MovieCardProps {
 export const MovieCard = ({ movie }: MovieCardProps) => {
 
   return (
-    
+    <div  className="movie-card">
+      <img src={movie.poster_path.includes("null") || movie.poster_path.includes("undefined") ? defaultPoster : movie.poster_path} alt={movie.title}/>
+      <div className="movie-info">
+        <h3>{movie.title || 'Título no disponible'}</h3>
+      </div>
+      <p>{movie.release_date || '-'}</p>
+    </div>
   );
 };
