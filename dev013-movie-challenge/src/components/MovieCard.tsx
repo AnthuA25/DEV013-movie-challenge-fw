10416580080ab7a67a42b3e20bfa01e1ba95b1e9
Aylin <aylin_santacruz@hotmail.com>
2024-08-21 00:18:51 -0500

    feat:Add MovieCardProps interface to define movie card properties

---
+++ dev013-movie-challenge/src/components/MovieCard.tsx
@@ -0,0 +1,14 @@
+import { Movie } from "../model/Movie";
+import defaultPoster from "../assets/default-movie.jpg";
+
+
+interface MovieCardProps {
+  movie: Movie;
+}
+
+export const MovieCard = ({ movie }: MovieCardProps) => {
+
+  return (
+    
+  );
+};
