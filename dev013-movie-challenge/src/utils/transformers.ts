78098dc57b45b4b68f599715cdebcddd397d4797
Aylin <aylin_santacruz@hotmail.com>
2024-08-21 00:13:10 -0500

    feat:create interface MovieData

---
+++ dev013-movie-challenge/src/utils/transformers.ts
@@ -0,0 +1,20 @@
+
+import { Genre } from '../service/movieService';
+import { Movie } from '../model/Movie';
+
+
+export interface MovieData {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    description: string;
+}
+
