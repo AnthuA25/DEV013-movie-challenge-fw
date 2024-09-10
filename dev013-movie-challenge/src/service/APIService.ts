18354eea60b89d4a038d9ce155122233c71d7c48
Aylin <aylin_santacruz@hotmail.com>
2024-09-10 16:15:49 -0500

    feat: Add filters and pagination

--- dev013-movie-challenge/src/service/APIService.ts
+++ dev013-movie-challenge/src/service/APIService.ts
@@ -2,6 +2,21 @@ import { apiKey} from "../modules/Api";
 import { Movie } from "../model/Movie";
 import { formatMovie, MovieData } from "../utils/transformers";
 
+interface Filters {
+  page?: number;
+  genreId?: number | null;
+  sortBy?: string | null;
+  searchQuery?:string;
+}
+export interface Results {
+  metaData: {
+    pagination: {
+      currentPage: number;
+      totalPages: number;   
+    };
+  };
+  movies: Movie[];
+}
 
 export const getNowPlaying = async (): Promise<Movie[]> => {
   const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?language=es-ES&page=1`, {
@@ -20,7 +35,32 @@ export const getNowPlaying = async (): Promise<Movie[]> => {
   return movies.slice(0, 3);
 };
 
-
+export const getMovie = async (filters:Filters,genres: Map<number, string>): Promise<Results> => {
+  const filter = filters.genreId ? `&with_genres=${filters.genreId}` : "";
+  const sort = filters.sortBy ? `&sort_by=${filters.sortBy}` : "";
+  const search = filters.searchQuery ? `&query=${filters.searchQuery}` : "";
+  const url =`https://api.themoviedb.org/3/discover/movie?language=es-ES&page=${filters.page}${filter}${sort}${search}`;
+  const resp = await fetch(url, {
+      headers: {
+        "Authorization": `Bearer ${apiKey}`
+      }
+  });
+  if (!resp.ok) {
+    throw new Error('Network response was not ok');
+  }
+  const data = await resp.json(); 
+  const movies = data.results.map((movie: MovieData) => formatMovie(movie, genres));
+  const result = {
+    metaData: {
+      pagination: {
+        currentPage: data.page,
+        totalPages: data.total_pages,
+      },
+    },
+    movies,
+  };
+  return result;
+}
 export const getTopMovie = async (): Promise<Movie[]> => {
   const resp = await fetch(`https://api.themoviedb.org/3/discover/movie?language=es-ES`, {
       headers: {
