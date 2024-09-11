a8693c9ec8c57cc317ad57aa9e88fbd1c11abad4
Aylin <aylin_santacruz@hotmail.com>
2024-09-10 21:47:28 -0500

    feat:Add search

---
+++ dev013-movie-challenge/src/components/Search.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { IoSearchOutline } from "react-icons/io5";
+import "../styles/Search.css";
+
+interface SearchProps {
+  onSearch: (query: string) => void;
+}
+
+export const Search = ({ onSearch }: SearchProps) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = (event: React.FormEvent) => {
+    event.preventDefault();
+    onSearch(searchQuery);
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="search-filter">
+      <p>Buscar</p>
+      <form onSubmit={handleSearch} className="search-input">
+        <input
+          type="text"
+          placeholder="Buscar"
+          value={searchQuery}
+          onChange={handleInputChange}
+        />
+        <button type="submit">
+          <IoSearchOutline />
+        </button>
+      </form>
+    </div>
+  );
+};
