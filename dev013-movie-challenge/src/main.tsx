48a1ff5b4d96cb6ed4cf1cf4aca26fbe27acff6e
Aylin <aylin_santacruz@hotmail.com>
2024-09-11 13:03:03 -0500

    feat:Add routes and Main

--- dev013-movie-challenge/src/main.tsx
+++ dev013-movie-challenge/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './Home.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Home.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import { MovieDetail } from "./components/MovieDetail.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/movie/:id" element={<MovieDetail/>}/>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
