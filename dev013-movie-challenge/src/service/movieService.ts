dbb7b6aca095388d6f457a7b7acd4e0a8d22ac19
Aylin <aylin_santacruz@hotmail.com>
2024-09-14 17:34:01 -0500

    feta: Add fetch function to retrieve movie genres from movie API

--- dev013-movie-challenge/src/service/movieService.ts
+++ dev013-movie-challenge/src/service/movieService.ts
@@ -12,3 +12,17 @@ export interface Actor {
     name: string;
     profile_path: string | null;
 }
+
+export const getMovieGenres = async (): Promise<Genre[]> => {
+    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?language=es-ES`, {
+        headers: {
+            "Authorization": `Bearer ${apiKey}`
+        }
+    });
+    if (!response.ok) {
+        throw new Error('Failed to fetch movie genres');
+    }
+    const data = await response.json();
+    return data.genres;
+
+}
