7d2b56054ce7952ff26a9b8d9bd047db90726481
Aylin <aylin_santacruz@hotmail.com>
2024-09-14 15:58:33 -0500

    Add pagination component to Home component

---
+++ dev013-movie-challenge/src/components/Pagination.tsx
@@ -0,0 +1,75 @@
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import "../styles/Pagination.css";
+
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
+}
+
+export const Pagination = ({
+  totalPages,
+  currentPage,
+  setCurrentPage,
+}: PaginationProps) => {
+  const getPagination = () => {
+    const pages = [];
+    const maxVisiblePages = 3; // Número de páginas visibles antes de los puntos suspensivos
+
+    for (let i = 1; i <= totalPages; i++) {
+      if (
+        i <= maxVisiblePages || // Primeras páginas
+        i > totalPages - maxVisiblePages || // Últimas páginas
+        (i >= currentPage - 1 && i <= currentPage + 1) // Páginas cercanas a la actual
+      ) {
+        pages.push(i);
+      } else if (pages[pages.length - 1] !== "...") {
+        pages.push("...");
+      }
+    }
+    if (pages[pages.length - 1] !== totalPages && totalPages > 1) {
+        pages.push(totalPages);
+      }
+
+    return pages;
+  };
+
+  const handlePageClick = (page: number | string) => {
+    if (typeof page === "number") {
+      setCurrentPage(page);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <div className="pagination">
+      <button onClick={handlePrev} disabled={currentPage === 1}>
+        <IoIosArrowBack />
+      </button>
+      {getPagination().map((page, index) => (
+        <button
+          key={index}
+          onClick={() => handlePageClick(page)}
+          className={page === currentPage ? "active" : ""}
+          disabled={page === "..." || page === currentPage}
+        >
+          {page}
+        </button>
+      ))}
+      <button onClick={handleNext} disabled={currentPage === totalPages}>
+        <IoIosArrowForward />
+      </button>
+    </div>
+  );
+};
